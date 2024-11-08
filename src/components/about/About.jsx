5e870524543f7090fb9dec6963e976acdecfe4b0
Griffin <baxgriffin@gmail.com>
2024-11-08 16:46:15 +0300

    first commit

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -12,6 +12,12 @@ const About = () => {
         <p>Discover a creative Wedding Photography and Videography hub nestled in Nairobi, Kenya. We are dedicated to crafting timeless wedding memories.</p>
         <Link to="/about" className="learn-more-button">Learn More &rarr;</Link>
       </div>
+      <div className="how-we-work">
+      <h3>How We Work</h3>
+    <p>Top Bride Photography and filming works in a logical and experienced approach by securing contracts through client inquiries or outreach from most social media platforms or phone calls.</p>
+    <p>Upon agreement, we schedule sessions, provide equipment, and offer creative direction.</p>
+    <p>After the shoot, we edit and deliver final content, ensuring client satisfaction and maintaining professional standards.</p>
+  </div>
     </div>
   );
 };
